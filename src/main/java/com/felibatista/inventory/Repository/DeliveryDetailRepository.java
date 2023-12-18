a48b99892e3f1b5bc680db9990924ee491f3bfaf
Felipe Batista <hellojavaa@gmail.com>
2023-12-18 13:08:43 -0300

    Update DeliveryDetailRepository.java

--- src/main/java/com/felibatista/inventory/Repository/DeliveryDetailRepository.java
+++ src/main/java/com/felibatista/inventory/Repository/DeliveryDetailRepository.java
@@ -1,7 +1,8 @@
 package com.felibatista.inventory.Repository;
 
+import com.felibatista.inventory.Entity.DeliveryDetail;
 import org.springframework.data.repository.CrudRepository;
 
-public interface DeliveryDetailRepository extends CrudRepository<DeliveryDetailRepository, Long> {
-    DeliveryDetailRepository findById(long id);
+public interface DeliveryDetailRepository extends CrudRepository<DeliveryDetail, Long> {
+    DeliveryDetail findById(long id);
 }
