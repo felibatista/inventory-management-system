a2223f73c7571080a09a94f0ab39d797021f457b
Felipe Batista <hellojavaa@gmail.com>
2023-12-19 09:41:43 -0300

    refactor: permit all requests

--- src/main/java/com/felibatista/inventory/security/WebSecurityConfig.java
+++ src/main/java/com/felibatista/inventory/security/WebSecurityConfig.java
@@ -59,8 +59,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/auth/**").permitAll()
-                                .requestMatchers("/api/users/**").permitAll()
+                        auth.requestMatchers("/api/*/**").permitAll()
                                 .anyRequest().authenticated()
                 );
 
